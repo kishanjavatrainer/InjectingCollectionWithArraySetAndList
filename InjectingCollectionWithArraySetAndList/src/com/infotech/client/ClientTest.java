03b0e1cc8b981839a6ad1580e081c55eb8d7baa9
Kishan Kumar <Kishan.Kumar@ingramcontent.com>
2017-03-07 18:58:39 +0530

    InjectingCollectionWithArraySetAndList source code

---
+++ InjectingCollectionWithArraySetAndList/src/com/infotech/client/ClientTest.java
@@ -0,0 +1,41 @@
+package com.infotech.client;
+
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.infotech.info.OrgInfo;
+
+public class ClientTest {
+
+	public static void main(String[] args) {
+
+		AbstractApplicationContext ctx = new ClassPathXmlApplicationContext("Beans.xml");
+		
+		OrgInfo orgInfo = ctx.getBean("orgInfo", OrgInfo.class);
+		
+		String[] namesArr = orgInfo.getNamesArr();
+		
+		for (String name : namesArr) {
+			System.out.println(name);
+		}
+		
+		System.out.println("-------------------------------------");
+		
+		List<String> empNameList = orgInfo.getEmpNameList();
+		
+		for (String name : empNameList) {
+			System.out.println(name);
+		}
+		System.out.println("-------------------------------------");
+		
+		Set<Integer> empIdsSet = orgInfo.getEmpIdsSet();
+		for (Integer id : empIdsSet) {
+			System.out.println(id);
+		}
+		ctx.close();
+	}
+
+}
